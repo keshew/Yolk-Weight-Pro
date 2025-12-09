8b6bf171f7b2b3f8b391b334f7b3a62e13f536cd
keshew <artyom.korotckov2017@yandex.ru>
2025-12-09 22:21:20 +0500

    Initial Commit

---
+++ Yolk Weight Pro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yolk Weight Pro
+//
+//  Created by Артём Коротков on 09.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yolk Weight Pro/Yolk_Weight_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yolk_Weight_ProApp.swift
+//  Yolk Weight Pro
+//
+//  Created by Артём Коротков on 09.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Yolk_Weight_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
